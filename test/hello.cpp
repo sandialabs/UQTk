2c5fba418f7baf0ff2dc9e795938100f3f016fd7
K Johnston <kjohnst@sandia.gov>
2019-08-29 10:53:26 -0700

    adding testing files copied form import branch

---
+++ test/hello.cpp
@@ -0,0 +1,17 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include<iostream>
+#include <cvode/cvode.h>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout<<"Hello World\n";
+
+    return 0;
+}
